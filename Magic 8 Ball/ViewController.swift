fce2f885668598fcdbc8daff9ff3bf693b60608f
Samuel Olaegbe <olaegbesamuel@gmail.com>
2023-02-25 18:10:05 +0100

    Initial Commit

---
+++ Magic 8 Ball/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Magic 8 Ball
+//
+//  Created by Samuel Olaegbe on 25/02/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
